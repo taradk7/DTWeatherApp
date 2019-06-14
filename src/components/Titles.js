669256223e7dce43217abcdb89c625897aadb076
taradk7 <40745748+taradk7@users.noreply.github.com>
2019-06-14 13:25:48 -0400

    Add files via upload

---
+++ src/components/Titles.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Titles extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <h1 className="title-container__title">Weather Finder</h1>
+                <h3 className="title-container__subtitle">Find the weather conditions in your area!</h3>
+                <h4 className="title-container__subtitle">by Dillon Tara</h4>
+            </div>
+
+        );
+    }
+};
+
+
+export default Titles;
